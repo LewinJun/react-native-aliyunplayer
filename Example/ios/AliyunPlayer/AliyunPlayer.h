65ffc60e97fb7b9d27fff7fdbdf8b7c8907f579e
zlk <89181292+zlk080@users.noreply.github.com>
2021-08-19 15:00:06 +0800

    iOS增加aliplayer封装类

---
+++ Example/ios/AliyunPlayer/AliyunPlayer.h
@@ -0,0 +1,61 @@
+//
+//  AliyunPlayer.h
+//  RNAliPlayer
+//
+//  Created by zlk on 2021/8/17.
+//
+
+#import <UIKit/UIKit.h>
+#import <React/RCTComponent.h>
+#import <AliyunPlayer/AliyunPlayer.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AliyunPlayer : UIView<AVPDelegate>
+
+//播放器对象
+@property(nonatomic,strong) AliPlayer * player;
+
+//定义要暴露属性
+@property(nonatomic,strong) NSString * source;
+@property(nonatomic,assign) BOOL  setAutoPlay;
+@property(nonatomic,assign) BOOL  setLoop;
+@property(nonatomic,assign) BOOL  setMute;
+@property(nonatomic,assign) BOOL  enableHardwareDecoder;
+@property(nonatomic,assign) float  setVolume;
+@property(nonatomic,assign) float  setSpeed;
+@property(nonatomic,strong) NSString * setReferer;
+@property(nonatomic,strong) NSString * setUserAgent;
+@property(nonatomic,assign) int  setMirrorMode;
+@property(nonatomic,assign) int  setRotateMode;
+@property(nonatomic,assign) int  setScaleMode;
+@property(nonatomic,strong) NSArray * configHeader;
+@property(nonatomic,assign) int  selectBitrateIndex;
+
+//定义要暴露的事件
+@property (nonatomic, copy) RCTBubblingEventBlock onAliCompletion;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliError;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliLoadingBegin;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliLoadingProgress;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliLoadingEnd;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliPrepared;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliRenderingStart;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliSeekComplete;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliCurrentPositionUpdate;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliBufferedPositionUpdate;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliAutoPlayStart;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliLoopingStart;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliBitrateReady;
+@property (nonatomic, copy) RCTBubblingEventBlock onAliBitrateChange;
+
+-(void)startPlay;
+-(void)pausePlay;
+-(void)stopPlay;
+-(void)reloadPlay;
+-(void)restartPlay;
+-(void)destroyPlay;
+-(void)seekTo:(int) position;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/ios/AliyunPlayer/RNAliyunPlayerManager.h
@@ -0,0 +1,18 @@
+//
+//  AliyunPlayerManager.h
+//  RNAliPlayer
+//
+//  Created by zlk on 2021/8/17.
+//
+
+#import <UIKit/UIKit.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTBridgeModule.h>
+#import "AliyunPlayer.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNAliyunPlayerManager : RCTViewManager
+
+@end
+
+NS_ASSUME_NONNULL_END

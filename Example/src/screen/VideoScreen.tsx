d3a07d597800abc543955faa82ac05eaeae81938
Lewin <junxianalan@gmail.com>
2021-08-31 17:52:37 +0800

    other: 添加相关方法demo

--- Example/src/screen/VideoScreen.tsx
+++ Example/src/screen/VideoScreen.tsx
@@ -1,10 +1,19 @@
 import { useNavigation } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 import React, { Component, useRef } from "react";
-import { View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet, SafeAreaView } from "react-native";
 import AliPlayer from "../components/AliPlayer";
 
-export default class VideoScreen extends Component<StackScreenProps<any>, any> {
+export default class VideoScreen extends Component<StackScreenProps<any>, {
+    play: boolean
+}> {
+
+    state = {
+        play: true,
+    }
+
+    player?: AliPlayer;
+
     componentDidMount() {
         this.props?.navigation.setOptions({
             headerTitle: "视频播放"
@@ -12,15 +21,54 @@ export default class VideoScreen extends Component<StackScreenProps<any>, any> {
     }
 
     render() {
-        return <View style={{ flex: 1, padding: 30 , alignItems: "center"  }}>
-            <AliPlayer
-            style={{ width: 300, height: 300 }} 
-            source='https://d-appimg.doctopia.com.cn/video/1626170384423967.mp4'
-            setAutoPlay={true}
-            setLoop={true}
-        />
-        
-    </View>
+        const { play } = this.state
+        return <View style={{ flex: 1, display: 'flex' }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <AliPlayer
+                        ref={(e) => this.player = e}
+                        style={{ flex: 1 }}
+                        source='https://d-appimg.doctopia.com.cn/video/1626170384423967.mp4'
+                        setAutoPlay={true}
+                        setLoop={true}
+                        onAliCurrentPositionUpdate={(e) => {
+                            // console.log(e.nativeEvent)
+                        }}
+                        onAliPrepared={(e) => {
+                            console.log(e.nativeEvent)
+                        }}
+                    />
+                    <View style={styles.bottomView}>
+                        <TouchableOpacity onPress={() => {
+                            !play ? this.player?.startPlay() : this.player?.pausePlay()
+                            this.setState({ play: !play }, () => {
+
+                            })
+                        }}>
+                            <Text style={{ color: 'white' }}>{
+                                play ? '暂停' : '播放'
+                            }</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={() => {
+                            this.player?.seekTo(0.9)
+                        }}>
+                            <Text style={{ color: 'white' }}>跳转到</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </SafeAreaView>
+        </View>
     }
 
 }
+
+const styles = StyleSheet.create({
+    bottomView: {
+        height: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 0
+    }
+})
